0b3219898d13ad9d00538763f98d4e333013623f
Eduardo Leonne <chamascadentes@gmail.com>
2023-11-03 13:06:38 -0400

    Another small fix

--- web/front/src/views/admin/photo/index.js
+++ web/front/src/views/admin/photo/index.js
@@ -9,37 +9,15 @@ import MediaPie from "views/admin/default/components/MediaPie";
 import CountryCard from "views/admin/default/components/CountryCard";
 import CountryPie from "views/admin/default/components/CountryPie";
 import { secondsToDhms, formatBytes, formatNumbers } from '../../../assets/utils'
-import banner from "assets/img/auth/banner.png";
-import eduardo from "assets/img/avatars/eduardo.jpeg";
-import eliane from "assets/img/avatars/eliane.jpeg";
-import Banner from "views/admin/default/components/Banner";
 import FolderCard from "views/admin/default/components/FolderCard";
 import FolderPie from "views/admin/default/components/FolderPie";
-import axios from "axios";
 
 export default function ClassifiedMediaVolume(props) {
-
-    // const [state, setState] = useState({
-    //     'labels': null,
-    //     'values': null,
-    //     'total': 0
-    // })
-
-    // useEffect(() => {
-    //     axios.get(process.env.REACT_APP_BASE_URL + '/api/get-video-trend').then((response) => {
-    //         const res = {
-    //             'labels': getChartOptions(response.data.data.labels, chartId),
-    //             'values': getChartData(response.data.data.values, "Tempo em segs"),
-    //             'total': response.data.data.total
-    //         }
-    //         setState({ ...res })
-    //     })
-    // }, []);
 
     return (
         <>
             <SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} gap='20px' mb='20px'>
-                <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>   
+                {/* <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>   
                     
                     <Banner
                         gridArea='1 / 1 / 2 / 2'
@@ -59,7 +37,7 @@ export default function ClassifiedMediaVolume(props) {
                         videos='9700000000000000000'
                         usedSpace='274'
                     />
-                </SimpleGrid>
+                </SimpleGrid> */}
                 <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>  
                     <VideoTrend />
                     <PhotoTrend />                    
